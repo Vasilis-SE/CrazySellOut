91a112aeb7dab4c83a0f013237a920fa21826f46
AlcatrazGR <alcatrazgr0@gmail.com>
2016-01-29 10:40:28 +0200

    Producer interface start

---
+++ CrazySellOut/app/src/main/java/codebrains/crazysellout/Activities/MainProducerActivity.java
@@ -0,0 +1,74 @@
+package codebrains.crazysellout.Activities;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import codebrains.crazysellout.Adapters.ProducerTabsAdapter;
+import codebrains.crazysellout.R;
+
+public class MainProducerActivity extends ActionBarActivity implements  android.support.v7.app.ActionBar.TabListener {
+
+    private ViewPager tabsviewPager;
+    private ProducerTabsAdapter mTabsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_producer);
+
+        tabsviewPager = (ViewPager) findViewById(R.id.tabspager);
+        mTabsAdapter = new ProducerTabsAdapter(getSupportFragmentManager());
+        tabsviewPager.setAdapter(mTabsAdapter);
+
+        this.getSupportActionBar().setHomeButtonEnabled(false);
+        this.getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab addProductsTab = getSupportActionBar().newTab().setText("Add Products").setTabListener(this);
+        ActionBar.Tab myProductsTab = getSupportActionBar().newTab().setText("My Products").setTabListener(this);
+        ActionBar.Tab productsTab = getSupportActionBar().newTab().setText("Products").setTabListener(this);
+
+        getSupportActionBar().addTab(addProductsTab);
+        getSupportActionBar().addTab(myProductsTab);
+        getSupportActionBar().addTab(productsTab);
+
+        //This helps in providing swiping effect for v7 compat library
+        tabsviewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // TODO Auto-generated method stub
+                getSupportActionBar().setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
---
+++ CrazySellOut/app/src/main/java/codebrains/crazysellout/Adapters/ProducerTabsAdapter.java
@@ -0,0 +1,49 @@
+package codebrains.crazysellout.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import codebrains.crazysellout.Fragments.AddItemsFragment;
+import codebrains.crazysellout.Fragments.CreatAccountFragment;
+import codebrains.crazysellout.Fragments.GoogleMapsFragment;
+import codebrains.crazysellout.Fragments.LoginFragment;
+import codebrains.crazysellout.Fragments.ProducerItemsFragment;
+import codebrains.crazysellout.Fragments.ProductsListAvtivity;
+
+/**
+ * Created by Vasilhs on 1/29/2016.
+ */
+public class ProducerTabsAdapter extends FragmentStatePagerAdapter {
+
+    private int TOTAL_TABS = 3;
+
+    public ProducerTabsAdapter(FragmentManager fm) {
+        super(fm);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                return new AddItemsFragment();
+
+            case 1:
+                return new ProducerItemsFragment();
+
+            case 2:
+                return new ProductsListAvtivity();
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return TOTAL_TABS;
+    }
+
+}
